3103d9f99f6142e312a0bc608827907d8a0c56ab
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-05-20 00:52:40 +0530

    added layout to cart page

--- src/Pages/Cart.tsx
+++ src/Pages/Cart.tsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../redux/store";
 import EmptyCart from "../components/EmptyCart";
@@ -20,28 +20,43 @@ const Cart = () => {
     <Box w="100%" py="50px">
       {data.length === 0 && <EmptyCart />}
 
-      {loading ? (
-        <ProductSkeleton />
-      ) : (
-        <SimpleGrid
-          // border="1px solid black"
-          w={{ base: "80%", sm: "70%", md: "80%", lg: "70%" }}
-          minChildWidth={{
-            base: "180px",
-            sm: "200px",
-            md: "220px",
-            lg: "250px",
-          }}
-          m="auto"
-          spacing={10}
+      <Flex
+        border="5px solid green"
+        w={{ base: "95%", sm: "95%", md: "95%", lg: "80%", xl: "70%" }}
+        m="auto"
+        justifyContent={{ base: "center", sm: "space-around" }}
+        alignItems={{ base: "center", sm: "flex-start" }}
+        gap="10px"
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Box
+          h={{ base: "300px", md: "400px" }}
+          w={{ base: "90%", sm: "45%", md: "45%", lg: "40%" }}
+          bgColor="pink"
+          textAlign="center"
+          position={{ base: "static", sm: "sticky" }}
+          top={{ base: "none", sm: "80px" }}
         >
-          {data?.map((el: any) => (
-            <Box key={el._id}>
-              <CartProductCard {...el} />
-            </Box>
-          ))}
-        </SimpleGrid>
-      )}
+          <Heading size="md">Cart Details</Heading>
+        </Box>
+        {loading ? (
+          <ProductSkeleton />
+        ) : (
+          <SimpleGrid
+            border="1px solid black"
+            w={{ base: "90%", sm: "60%", md: "50%", lg: "60%" }}
+            columns={1}
+            // m="auto"
+            spacing={5}
+          >
+            {data?.map((el: any) => (
+              <Box key={el._id}>
+                <CartProductCard {...el} />
+              </Box>
+            ))}
+          </SimpleGrid>
+        )}
+      </Flex>
     </Box>
   );
 };
--- src/components/CartProductCard.tsx
+++ src/components/CartProductCard.tsx
@@ -1,43 +1,90 @@
+import React, { useState } from "react";
+import { StarIcon } from "@chakra-ui/icons";
 import {
+  Box,
   Button,
   Card,
-  CardBody,
-  CardFooter,
-  Heading,
+  Divider,
+  Flex,
+  HStack,
+  Icon,
   Image,
-  Stack,
+  Select,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
 
 const CartProductCard = ({ ...prod }) => {
   const { imageURL: img, DefaultCategoryLinkRewrite: brand, Price, qty } = prod;
+  const [count, setCount] = useState<number>(qty);
+
+  const handleQuantity = (val: "INC" | "DEC") => {
+    if (val === "INC") setCount((prev) => prev + 1);
+    else setCount((prev) => prev - 1);
+  };
+
   return (
     <Card
-      direction={{ base: "column", sm: "row" }}
-      overflow="hidden"
-      variant="outline"
+      border="0px solid red"
+      py="10px"
+      boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
     >
-      <Image
-        objectFit="cover"
-        maxW={{ base: "100%", sm: "200px" }}
-        src={img}
-        alt={brand}
-      />
+      <HStack>
+        <Icon as={StarIcon} color="pink" />
+        <Text fontStyle="italic">ROCK BOTTOM DEAL</Text>
+      </HStack>
+      <Divider variant="dashed" color="pink" />
 
-      <Stack>
-        <CardBody>
-          <Heading size="md">{brand}</Heading>
-          <Text py="2">Price : {Price}</Text>
-          <Text py="2">Qunatity : {qty}</Text>
-        </CardBody>
-
-        <CardFooter>
-          <Button variant="solid" colorScheme="blue">
-            Remove
+      <Flex
+        p="20px"
+        gap="2%"
+        justifyContent="space-evenly"
+        alignItems="center"
+        flexWrap="wrap"
+      >
+        <Image src={img} w={{ base: "80px", md: "90px" }} borderRadius="10px" />
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          border="0px solid black"
+        >
+          <Box p="10px">
+            <Text>{brand}</Text>
+            <Text>Blue Dress</Text>
+            <Text>Rs. {Price * count}</Text>
+            <Text textDecoration="line-through">
+              Rs.{(Math.floor(Math.random() * 1000) + Price) * count}
+            </Text>
+          </Box>
+        </Flex>
+        <HStack border="0px solid black">
+          <Text fontSize="14px">Size:</Text>
+          <Select size="sm" border="none" fontWeight="bold">
+            <option value="option1">S</option>
+            <option value="option1">XS</option>
+            <option value="option2">X</option>
+            <option value="option3">M</option>
+            <option value="option3">L</option>
+            <option value="option3">XL</option>
+          </Select>
+          <Text fontSize="14px">Qty: </Text>
+          <Text fontWeight="bold">{count}</Text>
+          <Button
+            isDisabled={count === 1}
+            size="xs"
+            colorScheme="pink"
+            onClick={() => handleQuantity("DEC")}
+          >
+            -
           </Button>
-        </CardFooter>
-      </Stack>
+          <Button
+            size="xs"
+            colorScheme="pink"
+            onClick={() => handleQuantity("INC")}
+          >
+            +
+          </Button>
+        </HStack>
+      </Flex>
     </Card>
   );
 };
